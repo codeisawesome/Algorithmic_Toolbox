30f774cd4127a95a679bacc622943949f73ae6fe
Rui Dong <rui.dong.jh@gmail.com>
2016-10-12 23:04:47 -0400

    greatest divdr works

--- greatest_divdr.rb
+++ greatest_divdr.rb
@@ -1,5 +1,20 @@
-n = gets.chomp
-m = gets.chomp
+num = gets.chomp.split().map{|e| e.to_i}
 
-def GCD (n , m)
-  
+def gcb(num)
+  n = num[0]
+  m = num[1]
+  if n == 0
+    puts m
+  end
+
+
+  while n % m != 0 do
+    old_m = m
+    m = n % m
+    n = old_m
+  end
+
+  puts m
+end
+
+gcb(num)
